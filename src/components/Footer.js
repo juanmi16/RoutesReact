2a518182b5e64267e5c3ae71907ecfa8f6657b0e
juanmi16 <jmangumor@gmail.com>
2022-06-17 13:31:04 -0600

    main

---
+++ src/components/Footer.js
@@ -0,0 +1,30 @@
+import React from "react";
+import { useHistory } from "react-router-dom";
+
+export default function Footer() {
+
+  // Get the history object
+
+  const goBack = () => {
+    // imperatively redirect back
+    history.goBack();
+
+  }
+
+  const goForward = () => {
+    // imperatively redirect forward
+    history.goForward();
+    
+  }
+  
+  const history = useHistory();
+  return (
+    <footer>
+      
+      <button onClick={goBack}>Back</button>
+      <button onClick={goForward}>Forward</button>
+    </footer>
+  );
+}
+
+
